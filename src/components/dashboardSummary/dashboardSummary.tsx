753e94f57c64897651f7b176ec197e794369d257
Rpra1 <rishabh.prabhakar1@gmail.com>
2025-06-11 03:28:17 +0530

    task dashboard

--- src/components/dashboardSummary/dashboardSummary.tsx
+++ src/components/dashboardSummary/dashboardSummary.tsx
@@ -55,6 +55,7 @@ const DashboardSummary = () => {
                     Go to Task Table
                 </Button>
             </div>
+            <h3>Dashboard Summary</h3>
             <Row gutter={[16, 16]} style={{ marginBottom: 24 }}>
                 <Col xs={24} sm={12} md={8}>
                     <Card title="Total Tasks" variant="outlined">
